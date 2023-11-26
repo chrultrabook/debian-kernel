c16b5dcae2098bf3f3e7cc39c7cf58fdc7426030
WeirdTreeThing <bradyn127@protonmail.com>
2023-11-26 00:44:08 -0500

    Initial commit

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION="6.6.2"
+
+ROOT="$PWD"
+TARBALL="linux-${VERSION}.tar.xz"
+TARBALLURL="https://cdn.kernel.org/pub/linux/kernel/v${VERSION:0:1}.x/${TARBALL}"
+
+curl -LO $TARBALLURL
+tar -xf $TARBALL
+cd linux-${VERSION}
+
+for patch in $(ls $ROOT/patches)
+do
+	patch -p1 < $ROOT/patches/$patch
+done
+
+cp ${ROOT}/config .config
+make olddefconfig
+make bindeb-pkg -j$(nproc) INSTALL_MOD_STRIP=1
